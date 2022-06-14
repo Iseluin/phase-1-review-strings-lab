951f7fb94672ba37abb5d9b6d42063a8e16cea15
iseluin <kutay.kagan.ozenn@gmail.com>
2022-06-14 19:24:12 +0300

    done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Kutay';
+
+const welcomeMessage = `Welcome to Flatbook, + ${currentUser} + !`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const initial = currentUser.charAt(0)
+
+const shortGreeting = `Welcome, ${initial} + !` 
